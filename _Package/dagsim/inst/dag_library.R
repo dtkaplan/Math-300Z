39303f1b36e073d4f61718cf9816b66fc291c1f1
Daniel Kaplan <dtkaplan@gmail.com>
2022-09-27 17:24:57 -0500

    Working on NTI 23

--- _Package/dagsim/inst/dag_library.R
+++ _Package/dagsim/inst/dag_library.R
@@ -2,19 +2,19 @@
 #'
 dag01 <- list(
   x ~ eps(),
-  y ~ x + eps()
+  y ~ 1.5*x + 4.0 + eps()
 )
 
 dag02 <- list(
   x ~ eps(),
   a ~ eps(),
-  y ~ x + a + eps()
+  y ~ 3*x - 1.5*a + 5 +  eps()
 )
 
 dag03 <- list(
-  .c ~ eps(),
-  x ~ .c + eps(),
-  y ~ .c + eps()
+  g ~ eps(),
+  x ~ 1.0*g + eps(),
+  y ~ 1.0*g + eps()
 )
 
 save(dag01, dag02, dag03,
