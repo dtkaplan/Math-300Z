580c7419e61639e9b0364fd770c27679e4d350cb
Daniel Kaplan <dtkaplan@gmail.com>
2022-10-01 18:49:24 -0500

    Lesson 25 on in- versus out-of-sample error.

---
+++ _startup.R
@@ -0,0 +1,11 @@
+# load packages needed for the reading notes and NTIs
+
+library(knitr)
+library(kableExtra)
+library(ggformula) # Change plots to ggplot2
+library(tibble)
+library(dplyr)
+library(mosaic)
+library(mosaicModel)
+library(dagsim)
+knitr::opts_chunk$set(echo = TRUE)
