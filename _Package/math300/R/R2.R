46213eec6e4487f6431e117b50fbeec299b4586e
Daniel Kaplan <dtkaplan@gmail.com>
2022-12-04 14:28:00 -0600

    Finished draft of Lesson 29. Added adjusted R2 to the R2() function.

--- _Package/math300/R/R2.R
+++ _Package/math300/R/R2.R
@@ -8,9 +8,9 @@
 R2 <- function(mod) {
   r2 <- mosaic::rsquared(mod)
   k <- length(na.omit(mod$coefficients)) - 1
-  n <- k + mod$df.residual
-  f <- ((r2)/k) / ((1-r2)/(n-k))
-
-  data.frame(n=n, k=k, Rsquared=r2, F=f)
+  n <- k + 1 + mod$df.residual
+  f <- ((r2)/k) / ((1-r2)/(n-k-1))
+  adj <- 1 - (1 - r2)*(n-1)/(n-k-1)
+  data.frame(n=n, k=k, Rsquared=r2, F=f, adjR2=adj)
 }
 
